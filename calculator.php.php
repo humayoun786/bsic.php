66a971b84bc073c9f0bfc78d7b20cdf3585487de
humayoun786 <146459349+humayoun786@users.noreply.github.com>
2023-09-29 21:02:15 +0500

    Add files via upload

---
+++ calculator.php.php
@@ -0,0 +1,18 @@
+<?php
+$num_1=10;
+$num_2=2;
+$opr="+";
+
+if($opr=="+")
+echo $num_1 +$num_2;
+elseif($opr=="-")
+echo $num_1 -$num_2;
+elseif($opr=="*")
+echo $num_1 *$num_2;
+elseif($opr=="/")
+echo $num_1 /$num_2;
+elseif($opr=="%")
+echo $num_1 %$num_2;
+else
+echo" invalid choice"
+?>
